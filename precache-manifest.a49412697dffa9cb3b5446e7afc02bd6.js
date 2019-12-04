34791cc61f0afa9d4485c34d2f693de912f6636c
HyunIn Sim <>
2019-12-04 09:26:16 +0900

    Updates

---
+++ precache-manifest.a49412697dffa9cb3b5446e7afc02bd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f138a3d09b07f4abe162fb2cab746731",
+    "url": "/my_movie_app/index.html"
+  },
+  {
+    "revision": "8ff42f66787209075f91",
+    "url": "/my_movie_app/static/css/main.ba20093f.chunk.css"
+  },
+  {
+    "revision": "82df04b2c29aa431e496",
+    "url": "/my_movie_app/static/js/2.109d1774.chunk.js"
+  },
+  {
+    "revision": "8ff42f66787209075f91",
+    "url": "/my_movie_app/static/js/main.1ae4e770.chunk.js"
+  },
+  {
+    "revision": "cd225694bd361b2c4e93",
+    "url": "/my_movie_app/static/js/runtime-main.93f65528.js"
+  }
+]);
